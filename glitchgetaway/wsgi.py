bc0ab771c766edf1f6d4ea1fd9dbd8899893911e
Jahnavi Bhagirath Patel <patel.jahnavi@northeastern.edu>
2025-04-28 23:12:43 -0400

    G2: Add auto-migration on startup

--- glitchgetaway/wsgi.py
+++ glitchgetaway/wsgi.py
@@ -14,3 +14,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'glitchgetaway.settings')
 
 application = get_wsgi_application()
+
+try:
+    from django.core.management import call_command
+    call_command('migrate')
+except Exception as e:
+    print(f"Migration failed: {e}")
